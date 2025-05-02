ce9825b7f949c8b14ae65011f655135f7db699a2
Dinesh Bobburu <bobburudinesh123@gmail.com>
2025-05-01 23:46:01 -0500

    Implemented Assembly codes to save psp, msp, disable and enable interrupts, pending pendSV, and checking task state and unblocking based on global tick count.

--- Src/main_Task_Scheduler.c
+++ Src/main_Task_Scheduler.c
@@ -31,6 +31,9 @@
 #define SYS_CLK		16000000U
 #define TICK_HZ		1000
 
+#define INTERRUPT_ENABLE() do{__asm volatile("MOV R0,0x0"); __asm volatile("MSR PRIMASK, R0");}while(0)
+#define INTERRUPT_DISABLE() do{__asm volatile("MOV R0,0x1"); __asm volatile("MSR PRIMASK, R0");}while(0)
+
 //#define TASK_STATE_READY		1
 //#define TASK_STATE_RUNNING
 //#define	TASK_STATE_BLOCKED
@@ -53,17 +56,23 @@ void task_4_Handler(void);
 
 void tasks_Init(void);
 void enable_Sys_Tick_Timer(void);
+void pend_PendSV(void);
+void unblock_tasks(void);
+void increment_Tick(void);
 
+__attribute__((naked))void scheduler_Stack_Init(uint32_t stack_start);
+__attribute((naked))void switch_sp_to_psp(void);
 
 typedef struct {
-	uint32_t	*top_Of_Stack;		// Current top of the stack, latest PSP value
+	uint32_t	top_Of_Stack;		// Current top of the stack, latest PSP value
 	uint32_t	block_count;		// The delay upto which task is blocked
 	Task_States_t	task_State;				// current state of the task, of it is ready or blocked.
 	void	    (*task_Handler)(void);	// Task handler for this task
 }TCB_t;	// Task Control Block Data Structure
 
-TCB_t user_Tasks[MAX_TASKS];
-
+TCB_t user_Tasks[MAX_TASKS];	// These are the user tasks
+uint8_t current_Task = 1;	// This variable holds current running task id
+uint32_t global_Tick_Count = 0; // Global tick count to keep track of time elapsed using systick interupt
 int main(void) {
 
 	enable_Faults();
@@ -147,14 +156,14 @@ void tasks_Init(void) {
 	user_Tasks[3].task_Handler = task_3_Handler;
 	user_Tasks[4].task_Handler = task_4_Handler;
 
-	user_Tasks[0].top_Of_Stack = (uint32_t*)TASK_IDLE_STACK_START;
-	user_Tasks[1].top_Of_Stack = (uint32_t*)TASK_1_STACK_START;
-	user_Tasks[2].top_Of_Stack = (uint32_t*)TASK_2_STACK_START;
-	user_Tasks[3].top_Of_Stack = (uint32_t*)TASK_3_STACK_START;
-	user_Tasks[4].top_Of_Stack = (uint32_t*)TASK_4_STACK_START;
+	user_Tasks[0].top_Of_Stack = (uint32_t)TASK_IDLE_STACK_START;
+	user_Tasks[1].top_Of_Stack = (uint32_t)TASK_1_STACK_START;
+	user_Tasks[2].top_Of_Stack = (uint32_t)TASK_2_STACK_START;
+	user_Tasks[3].top_Of_Stack = (uint32_t)TASK_3_STACK_START;
+	user_Tasks[4].top_Of_Stack = (uint32_t)TASK_4_STACK_START;
 
 	for(int i = 0; i < MAX_TASKS; i++) {
-		uint32_t *pPSP = user_Tasks[i].top_Of_Stack;
+		uint32_t *pPSP = (uint32_t*)user_Tasks[i].top_Of_Stack;
 		pPSP--;
 		*pPSP = XPSR_DEFAULT;	// XPSR = default XPSR value
 
@@ -168,7 +177,7 @@ void tasks_Init(void) {
 			pPSP--;
 			*pPSP = 0;
 		}
-		user_Tasks[i].top_Of_Stack = pPSP;	// Save the value of top of stack of that task(PSP)
+		user_Tasks[i].top_Of_Stack = (uint32_t)pPSP;	// Save the value of top of stack of that task(PSP)
 	}
 }
 
@@ -188,5 +197,62 @@ void enable_Sys_Tick_Timer(void) {
 }
 
 void SysTick_Handler(void) {
+	increment_Tick();	// increment global tick count
+	unblock_tasks();	// check if there are any tasks to be unblocked once their delay count is completed.
+	pend_PendSV();	// pend pendSV
 
 }
+
+void increment_Tick(void) {
+	global_Tick_Count++;
+}
+void task_Delay(uint32_t	delay_in_ticks){
+	INTERRUPT_DISABLE();	// disable the interrupts
+	user_Tasks[current_Task].block_count = global_Tick_Count+delay_in_ticks; // task will be blocked for (global_Tick_Count+delay_in_ticks) ticks
+	user_Tasks[current_Task].task_State = blocked;
+
+	INTERRUPT_ENABLE();		// enable the interrupts
+}
+
+void pend_PendSV(void) {
+	uint32_t *pICSR = (uint32_t*)0xE000ED04;
+	*pICSR |= (1<<28); // Pend the pend SV
+}
+
+void unblock_tasks(void) {
+	for(uint8_t i = 1; i<MAX_TASKS; i++) {
+		if(user_Tasks[i].task_State != ready || user_Tasks[i].task_State != running) {
+			if(global_Tick_Count == user_Tasks[i].block_count) {
+				user_Tasks[i].task_State = ready;
+			}
+		}
+	}
+}
+
+// Stack pointer related functions START
+
+__attribute__((naked))void scheduler_Stack_Init(uint32_t stack_start) {
+	__asm volatile("MSR MSP, %0": :"r"(stack_start));
+	__asm volatile("BX LR");
+}
+
+uint32_t get_current_task_psp(void) {
+	return user_Tasks[current_Task].top_Of_Stack;
+}
+__attribute((naked))void switch_sp_to_psp(void) {
+	__asm volatile("PUSH {LR}");
+	__asm volatile("BL get_current_task_psp");
+	__asm volatile("MSR PSP, R0");
+	__asm volatile("MOV R0, #0x2");
+	__asm volatile("MSR CONTROL, R0");
+	__asm volatile("POP {LR}");
+	__asm volatile("BX LR");
+}
+
+void save_current_task_psp(uint32_t current_stack_position) {
+	user_Tasks[current_Task].top_Of_Stack = current_stack_position;
+}
+
+
+
+// Stack pointer related functions END
